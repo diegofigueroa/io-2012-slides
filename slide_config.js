ebeceac91331dc37b5daad9c745cd0d4e01e0ea9
Diego Figueroa <diegofigueroa99@gmail.com>
2013-06-23 15:47:31 -0600

    first release.

--- slide_config.js
+++ slide_config.js
@@ -1,7 +1,7 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
+    title: 'Introducción',
     subtitle: 'Ciencias de la Computaci&oacute;n IV',
     eventTitle: '2013',
     useBuilds: true, // Default: true. False will turn off slide animation builds.
